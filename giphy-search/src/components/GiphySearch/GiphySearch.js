ddb11a13521c7a05e79821e43d781d6189a5c867
Priscila-Flores <priscilauchoaa@gmail.com>
2022-12-14 17:32:25 -0300

    add search in giphy api

--- giphy-search/src/components/GiphySearch/GiphySearch.js
+++ giphy-search/src/components/GiphySearch/GiphySearch.js
@@ -1,13 +1,36 @@
+import GiphyList from '../GiphyList/GiphyList';
 import SearchButton from '../SearchButton/SearchButton';
 import './GiphySearch.css';
 import { ReactComponent as SearchIcon } from './search.svg';
+
+let APIKEY = 'XsI4tsmH0t4FjJ0SXp6nVIkDN6mXD2GS';
+
+function searchGif(e) {
+  let url = `https://api.giphy.com/v1/gifs/search?api_key=${APIKEY}&limit=12&q=${e.target.value}`;
+  console.log('url-->', url);
+
+  fetch(url)
+    .then((res) => res.json())
+    .then((content) => {
+      console.log(content.data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
 function GiphySearch(props) {
   return (
     <div className='container-giphy-list'>
       <SearchIcon />
-      <input type='text' name='name' value='search'></input>
+      <input
+        type='text'
+        name='name'
+        // value='search'
+        onChange={(e) => searchGif(e)}
+      ></input>
       <SearchButton text='X' />
+      <GiphyList />
     </div>
   );
 }
